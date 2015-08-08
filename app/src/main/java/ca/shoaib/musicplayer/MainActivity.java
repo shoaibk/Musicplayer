556659c7d6dfd6a8c351b05a25f54cabc3abe457
Shoaib Khan <shb.khan@gmail.com>
2015-08-07 23:40:37 -0400

    Add Adapter

--- app/src/main/java/ca/shoaib/musicplayer/MainActivity.java
+++ app/src/main/java/ca/shoaib/musicplayer/MainActivity.java
@@ -1,16 +1,58 @@
 package ca.shoaib.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        songView = (ListView) findViewById(R.id.song_list);
+        songList = new ArrayList<>();
+        getSongList();
+        Collections.sort(songList, new Comparator<Song>() {
+            @Override
+            public int compare(Song lhs, Song rhs) {
+                return lhs.getTitle().compareTo(rhs.getTitle());
+            }
+        });
+        SongAdapter songAdt = new SongAdapter(this, songList);
+        songView.setAdapter(songAdt);
+    }
+
+    public void getSongList() {
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        if(musicCursor != null && musicCursor.moveToFirst()) {
+            // get columns
+            int titleColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            // add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            } while (musicCursor.moveToNext());
+        }
     }
 
     @Override
@@ -26,11 +68,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/ca/shoaib/musicplayer/SongAdapter.java
@@ -0,0 +1,53 @@
+package ca.shoaib.musicplayer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends BaseAdapter {
+    private ArrayList<Song> songs;
+    private LayoutInflater songInf;
+
+    public SongAdapter(Context c, ArrayList<Song> theSongs) {
+        songs = theSongs;
+        songInf = LayoutInflater.from(c);
+    }
+
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // map to Song layout
+        LinearLayout songLay = (LinearLayout) songInf.inflate(R.layout.song, parent, false);
+        TextView songView = (TextView) songLay.findViewById(R.id.song_title);
+        TextView artistView = (TextView) songLay.findViewById(R.id.song_artist);
+
+        // get current song position
+        Song currSong = songs.get(position);
+        songView.setText(currSong.getTitle());
+        artistView.setText(currSong.getArtist());
+        // set position as tag
+        songLay.setTag(position);
+        return songLay;
+    }
+}
